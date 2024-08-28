31ec31a805abc7cc12181f2af33100e82b7eefac
Amy Wright <amywrightmail@protonmail.com>
2024-08-27 21:27:02 -0700

    make sure ipx fits within v_256x256

--- _mcd/src/ipx_layout.s
+++ _mcd/src/ipx_layout.s
@@ -4,13 +4,10 @@
 // ROM will begin at the start of Work RAM, overwriting the security code/IP
 GLOBAL MODULE_ROM_ORIGIN _WRKRAM
 
-// RAM will go right after ROM
-GLOBAL MODULE_RAM_ORIGIN 0xFFF500
+GLOBAL MODULE_RAM_ORIGIN 0xFFA200
 
-// that gives us a litte over 61KB for ROM space...
 GLOBAL MODULE_ROM_LENGTH, MODULE_RAM_ORIGIN - MODULE_ROM_ORIGIN
 
-// ... and 512 bytes of RAM. This layout is rather arbitrary.
 GLOBAL MODULE_RAM_LENGTH, 0x200
 
 // Finally, we must specify from where the code will actually execute, which is
